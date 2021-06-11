4bc46bca2f65fa5910e76345a4d1956d43bd9c36
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-06-11 19:53:20 -0300

    Set moment locale

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
  */
 
 import 'react-native-gesture-handler';
+import 'moment/locale/pt-br';
+
+import moment from 'moment';
 
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 
 import App from './App';
 
+moment.locale('pt-br');
 AppRegistry.registerComponent(appName, () => App);
