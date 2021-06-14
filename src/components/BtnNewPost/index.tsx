7a8c7c7718b0d6091969499d6e7d166f958004b5
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-06-14 20:20:42 -0300

    Added shadow

--- src/components/BtnNewPost/index.tsx
+++ src/components/BtnNewPost/index.tsx
@@ -2,11 +2,11 @@ import React, { memo } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Container } from './styles';
+import { Container, styles } from './styles';
 
 const BtnNewPost = (props: { onPress: (param: boolean) => void }) => {
   return (
-    <Container onPress={() => props.onPress(true)}>
+    <Container style={styles.shadow} onPress={() => props.onPress(true)}>
       <Icon name="plus" size={28} color="#fff" />
     </Container>
   );
--- src/components/BtnNewPost/styles.ts
+++ src/components/BtnNewPost/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 
+import { StyleSheet } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
@@ -16,3 +17,17 @@ export const Container = styled(RectButton)`
   border-radius: 100px;
   background-color: ${({ theme }) => theme.primary};
 `;
+
+export const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+  },
+});
