53d05171bd6d7e093d6995e717c7aa4e6806191c
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-06-14 19:07:59 -0300

    Changed Container styles

--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -2,6 +2,8 @@ import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  margin-bottom: 24px;
+
   background-color: ${({ theme }) => theme.inverseColor};
 `;
 
