8b6822a98d6e92a050453816bd4fbf4361b1594a
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-06-15 16:57:14 -0300

    Create update function

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,12 @@
 import api from './api';
 
-import { ApiReturnPosts } from '../@types/types';
+import { Platform } from 'react-native';
+
+import {
+  ApiReturnPosts,
+  ReturnApiUserUpdate,
+  UserServiceUpdateData,
+} from '../@types/types';
 
 class UserService {
   private readonly token: string | undefined;
@@ -17,6 +23,35 @@ class UserService {
     });
 
     return data as ApiReturnPosts;
+  };
+
+  update = async (data: UserServiceUpdateData) => {
+    const formData = new FormData();
+
+    data.name && formData.append('name', data.name);
+    data.email && formData.append('email', data.email);
+
+    if (data.image) {
+      const imageURL =
+        Platform.OS === 'android'
+          ? data.image.path
+          : data.image.path?.replace('file://', '');
+
+      formData.append('image', {
+        url: imageURL,
+        name: `${Date.now()}.${data.image.mime.split('/').reverse()[0]}`,
+        type: data.image.mime,
+      });
+    }
+
+    const { data: response } = await api.put('users', formData, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response as ReturnApiUserUpdate;
   };
 }
 
